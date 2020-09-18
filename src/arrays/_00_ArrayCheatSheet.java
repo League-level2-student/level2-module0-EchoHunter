7349cd870fba4a22468aa963d81f49ee83cab7e8
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2020-09-17 18:35:28 -0700

    :b: :dragon:
    robot race and stuff

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,54 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
+		Random r = new Random();
 		//1. make an array of 5 Strings
-		
+		String[] names = new String[5];
 		//2. print the third element in the array
-		
+		System.out.println(names[2]);
 		//3. set the third element to a different value
-		
+		names[2] = ("melissa");
 		//4. print the third element again
-		
+		System.out.println(names[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(names[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] num = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int b = 0; b < 50; b++) {
+			num[b]=r.nextInt(200);
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		int d = 1000;
+		for (int c = 0; c < 50; c++) {
+			if(num[c] < d) {
+				d = num[c];
+			}
+			
+		}
+		System.out.println(d);
 		//9 print the entire array to see if step 8 was correct
+		for (int b = 0; b < 50; b++) {
+			
 		
+		System.out.println(num[b]);
+		}
 		//10. print the largest number in the array.
+		int a = 0;
+		for (int v = 0; v < 50; v++) {
+			if(num[v] > a) {
+				a = num[v];
+			}
+			
+			
+		}
+		System.out.println(a);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,12 +1,21 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	
 
 	//2. create an array of 5 robots.
-
+ Robot[] r = new Robot[5];
+ Robot d = new Robot();
 	//3. use a for loop to initialize the robots.
 
+ for (int i = 0; i < 6; i++) {
+	 r[i]= new Robot();
+	r[i].moveTo(250+i*100, 500);
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
@@ -19,4 +28,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
